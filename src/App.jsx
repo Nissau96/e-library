ebed1cf8e3dec557796725c35bac9e2fc06f4010
Nissua96 <ihalidu996@gmail.com>
2026-01-04 20:33:25 +0000

    Setting up Project Infrastructure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from './components/Layouts/Sidebar'
+
+export default function App() {
+  return (
+    <div className='min-h-screen bg-linear-to-br from-slate-50 via-blue-50 to-indigo-50 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 transition-all duration-500'>
+      <div className="flex h-screen overflow-hidden">
+        <Sidebar />
+      </div>
+    </div>
+  )
+}
