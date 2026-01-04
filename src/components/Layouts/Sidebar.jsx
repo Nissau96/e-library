97d811989544abc4b97ed593a79f7dd6cb9b616d
Nissua96 <ihalidu996@gmail.com>
2026-01-04 22:00:00 +0000

    Designing Sidebar component

--- src/components/Layouts/Sidebar.jsx
+++ src/components/Layouts/Sidebar.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar-theme'>
+        {/* Logo */}
+        <div className="p-6 border-b border-slate-200/5 dark:border-slate-700/50">
+        <div className="flex items-center space-x-3">
+            <img src="/src/assets/images/MoFEP_Logo__3_-removebg-preview.png" alt="Logo" className="h-16 w-16"/>
+        </div>
+        </div>
+    </div>
   )
 }
