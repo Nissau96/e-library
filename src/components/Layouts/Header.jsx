e128c7ba1014fcfe61f090a778ec30d9e5ff865c
Nissau96 <ihalidu996@gmail.com>
2026-01-05 16:18:54 +0000

    Developing the Navigation Structure

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='bg-white/-80 dark:bg-slate-900/80 backdrop-blur-xl border-b border-slate-200/50 darker:border-slate-700/50 px-6 py-4'>
+        
+    </div>
   )
 }
